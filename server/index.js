c3db4120bdac396992ac9927b5d4c75a7ce2a19b
Gerald Maduabuchi <Mgeraldoj07@gmail.com>
2022-11-01 08:21:08 +0000

    push

--- server/index.js
+++ server/index.js
@@ -1,33 +1,38 @@
 // Import express
 const express = require('express');
-/*
-If this were react, it would have been:
-import express from 'express';
-because the export export syntax, works this way:
-
-const app = () => {
-    return (
-    <div></div>
-    )
-}
-
-export default App;
-
-
-But for the case in express:
-
-const express = require('express');
-because the method of export is
-
-
-modules.exports = express();
-*/
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const { application } = require('express');
 
 // Initializing our express app
 const app = express();
+
+
+var corsOptions = {
+    origin: "http://localhost:4000"
+};
+
+// Parse requests of content-type "application/json"
+app.use(cors(corsOptions));
+
+app.use(bodyParser.json());
+
+const db = require('./Model')
+db.sequelize.sync();
+
+application.use(bodyParser.urlencoded({ extended: true }))
+
+// Route
+app.get('/', (req, res) => {
+    res.json({ message: 'we are up!' })
+})
+
 
 // Assign a port number, where it would run
 const PORT = process.env.PORT || 5000;
 
 // Listen on the port
 app.listen(PORT, () => console.log(`Our Server is running on port ${PORT}`));
+
+
+
